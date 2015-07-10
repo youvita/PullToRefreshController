537a4dd2e000e63768ded7e67421e0f3d7a5fbff
Chan Youvita <chanyouvita@Youvita-iMac.local>
2015-07-10 11:07:22 +0700

    Initial Commit

---
+++ BehaviorUIKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BehaviorUIKit
+//
+//  Created by Chan Youvita on 7/10/15.
+//  Copyright (c) 2015 Kosign. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
